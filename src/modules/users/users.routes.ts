a884f9614c54c4711c65dee2490d2eb4fe5b8fc3
NatthasitW <93566770+natthasitwp@users.noreply.github.com>
2026-04-17 17:03:56 +0700

    feature: add registration list and cancel endpoints

--- src/modules/users/users.routes.ts
+++ src/modules/users/users.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { requireAuth, requireRole } from "../../middlewares/auth.js";
-import { getMe, updateMe, deleteMe } from "./../../controllers/user.controller.js";
+import {getMe,updateMe,deleteMe,getRegistrations,deleteRegistration} from "./../../controllers/user.controller.js";
 import { upload } from "./../../utils/uploads.js";
 
 export const usersRouter = Router();
@@ -12,3 +12,7 @@ usersRouter.get("/me", getMe);
 usersRouter.put("/me", upload.single("avatar"), updateMe);
 
 usersRouter.delete("/me", deleteMe);
+
+usersRouter.get("/registrations", getRegistrations);
+
+usersRouter.delete("/me/registrations/:eventId", deleteRegistration);
